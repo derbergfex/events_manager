c744f15599ed470f838fc4c3fa9dd72c8e34c5a9
Yussef Soudan <yussefsoudan@gmail.com>
2018-09-02 15:39:04 +0100

    first commit

---
+++ lib/event_manager.rb
@@ -0,0 +1,67 @@
+require 'csv'
+require 'google/apis/civicinfo_v2'
+require 'erb'
+require 'date'
+
+def legislators_by_zipcode(zip)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zip,
+      levels: 'country',
+      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+    ).officials
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def save_thank_you_letters(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts(form_letter)
+  end
+end
+
+def clean_number(num)
+  num_str = num.to_s
+  if (num_str =~ /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-.]?\d{3}[\s\-.]?\d{4}$/)
+    num_str.gsub!(/^1|[.\-\s\(\)]/, '')
+    if (num_str.length == 10)
+      num_str
+    else
+      "Invalid Number"
+    end
+  else
+    "Invalid Number"
+  end
+end
+
+puts "EventManager initialized."
+
+template_letter = File.read("form_letter.erb")
+erb_template = ERB.new(template_letter)
+
+contents = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+  phone_number = clean_number(row[:homephone])
+  
+  form_letter = erb_template.result(binding)
+  save_thank_you_letters(id, form_letter)
+end
+
+
+
